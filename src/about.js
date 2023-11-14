a69872790fb0cd751c33407ee03f2583a83dc5ae
crashofficial <crashofficial96@gmail.com>
2023-11-14 23:26:35 +0530

    first commit

---
+++ src/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Appp=() =>{
+return(
+<div style={{textAlign:'center'}}>
+    <p style={{ color:'#f9d3b4',bottom: '5px',left: '30px' }}> advertise-- @5$ per month</p>
+    <p style={{ color:'#f9d3b4',bottom: '5px',left: '30px' }}>   contact email--crashofficial96@gmail.com </p> 
+      
+</div>
+)
+}
+export default Appp;
