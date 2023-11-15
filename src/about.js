d3e56401e01ec53eb2944eadf09bb2c7b8ae60b3
crashmovies <swarnakarharshendu420@gmail.com>
2023-11-15 10:22:42 +0530

    user

--- src/about.js
+++ src/about.js
@@ -1,7 +1,23 @@
 import React from "react";
+
+const li='https://www.youtube.com/watch?v=fhr3MzT6exg'
+
 const Appp=() =>{
+    
+
+    
+
 return(
 <div style={{textAlign:'center'}}>
+<div>
+  <a
+    style={{ color: '#f9d3b4', bottom: '5px', left: '30px',cursor:'pointer' }}
+    onClick={() => window.open(li, '_blank', 'noopener noreferrer')}
+    
+  >
+    Click here to see how to watch movies
+  </a>
+</div>
     <p style={{ color:'#f9d3b4',bottom: '5px',left: '30px' }}> advertise-- @5$ per month</p>
     <p style={{ color:'#f9d3b4',bottom: '5px',left: '30px' }}>   contact email--crashofficial96@gmail.com </p> 
       
