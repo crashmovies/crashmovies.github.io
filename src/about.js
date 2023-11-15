abc8a3243c8eb899d5b853a6706cf87d2897f1f9
crashmovies <swarnakarharshendu420@gmail.com>
2023-11-15 11:25:28 +0530

    user

--- src/about.js
+++ src/about.js
@@ -1,27 +1,39 @@
 import React from "react";
 
-const li='https://www.youtube.com/watch?v=fhr3MzT6exg'
+const li = 'https://www.youtube.com/watch?v=fhr3MzT6exg';
 
-const Appp=() =>{
-    
+const Appp = () => {
+  const openLink = () => {
+    window.open(li, '_blank', 'noopener noreferrer');
+  };
 
-    
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <div>
+        <button
+          style={{
+            color: '#f9d3b4',
+            bottom: '5px',
+            left: '30px',
+            cursor: 'pointer',
+            border: 'none',
+            background: 'none',
+            padding: '0',
+            textDecoration: 'underline',
+          }}
+          onClick={openLink}
+        >
+          Click here to see how to watch movies
+        </button>
+      </div>
+      <p style={{ color: '#f9d3b4', bottom: '5px', left: '30px' }}>
+        advertise-- @5$ per month
+      </p>
+      <p style={{ color: '#f9d3b4', bottom: '5px', left: '30px' }}>
+        contact email--crashofficial96@gmail.com
+      </p>
+    </div>
+  );
+};
 
-return(
-<div style={{textAlign:'center'}}>
-<div>
-  <a
-    style={{ color: '#f9d3b4', bottom: '5px', left: '30px',cursor:'pointer' }}
-    onClick={() => window.open(li, '_blank', 'noopener noreferrer')}
-    
-  >
-    Click here to see how to watch movies
-  </a>
-</div>
-    <p style={{ color:'#f9d3b4',bottom: '5px',left: '30px' }}> advertise-- @5$ per month</p>
-    <p style={{ color:'#f9d3b4',bottom: '5px',left: '30px' }}>   contact email--crashofficial96@gmail.com </p> 
-      
-</div>
-)
-}
 export default Appp;
